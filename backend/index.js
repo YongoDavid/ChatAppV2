e702c73b10f3d163eec63ed7b43859e2850b713e
Yongo David <davidyongo9@gmail.com>
2025-05-24 22:18:47 +0100

    backend update

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chatappv2-3ex0.onrender.com/",
+    origin: "https://chatappv2-3ex0.onrender.com",
     credentials: true,
   })
 );
