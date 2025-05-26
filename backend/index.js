c3464b2d23bc4beaca10c172db23a7a5c0583e67
Yongo David <davidyongo9@gmail.com>
2025-05-26 22:23:13 +0100

    Local update

--- backend/index.js
+++ backend/index.js
@@ -18,19 +18,20 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "https://chatappv2-3ex0.onrender.com",
-    credentials: true,
-  })
-);
 
 // app.use(
 //   cors({
-//     origin: "http://localhost:5173",
+//     origin: "https://chatappv2-3ex0.onrender.com",
 //     credentials: true,
 //   })
 // );
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
