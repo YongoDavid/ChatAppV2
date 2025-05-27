2efbb8ada01cb78d413d1b295d3fbc28bf30fb6a
Yongo David <davidyongo9@gmail.com>
2025-05-27 02:26:15 +0100

    another front and back URL update again

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://chatappv2-erkt.onrender.com" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatappv2-1-dzy1.onrender.com" : "/";
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
