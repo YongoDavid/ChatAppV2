834542b1c6284760c521e5d2b1563fedae46cf70
Yongo David <davidyongo9@gmail.com>
2025-05-28 12:31:21 +0100

    F/B socket.io update

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,12 +3,8 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = ( import.meta.env.MODE === "development"
-  ? "http://localhost:5001"
-  : "https://chatappv2-erkt.onrender.com", {
-    withCredentials: true,
-  });
-  
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001": "https://chatappv2-erkt.onrender.com";
+
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
@@ -93,10 +89,12 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
+      withCredentials: true,
       query: {
         userId: authUser._id,
       },
     });
+
     socket.connect();
 
     set({ socket: socket });
